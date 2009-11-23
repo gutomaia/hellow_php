dd8bdff437939c757b076dbb81d73e8b1b1587c9
Guto Maia <guto@guto.net>
2009-11-23 17:13:35 -0300

    removing unecessary debug and comments

--- src/example/test.php
+++ src/example/test.php
@@ -1,6 +1,4 @@
 <?php
-echo $argc;
-var_dump($argv);
 if ($argc != 3){
 	die("usage: php test.php LOGIN PASSWORD\n\n");
 }
@@ -10,8 +8,6 @@ require_once ('../client.php');
 Client::init();
 
 $msn = new Hellow_Protocol_Msnp8;
-//$socket = new Hellow_Core_SocketConnection;
-//$msn->setConnectionHandle($socket);
 $msn->login ($argv[1], $argv[2]);
 
 
