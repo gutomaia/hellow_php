52c3f65c036d3f84c1f7008d725bcf178a08a055
Guto Maia <guto@guto.net>
2010-09-12 10:20:02 -0300

    Adding more use cases on the testProtocol

--- src/test/php/ProtocolTest.php
+++ src/test/php/ProtocolTest.php
@@ -56,13 +56,12 @@ class ProtocolTest extends PHPUnit_Framework_TestCase {
 	public function testSession(){
 		//Sends the MSN Client version
 		$this->send("VER 1 MSNP8 CVR0\r\n");
-		//acknolodge
 
+		//Acknowledge
 		$this->receive("VER 1 MSNP8 CVR0\r\n");
 		$this->send("CVR 2 0x0409 win 4.10 i386 MSNMSGR 6.0.0602 MSMSGS dvader@empire.com\r\n");
 		
 		//Client sends information
-
 		$this->receive("CVR 2 6.0.0602 1.0.000 http://download.microsoft.com/download/8/a/4/\r\n");
 		$this->send("USR 3 TWN I dvader@empire.com\r\n");
 
@@ -101,6 +100,7 @@ class ProtocolTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->_mockClient->group['group_id'], '1');
 		$this->assertEquals($this->_mockClient->group['name'], 'Jedis');
 
+		//Add Emperor as a contact
 		$this->assertEquals($this->_mockClient->contact, null);
 		$this->receive("LST emperor@empire.com Emperor 13 0\r\n");
 		$this->assertEquals($this->_mockClient->contact['user'], 'emperor@empire.com');
@@ -109,6 +109,7 @@ class ProtocolTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($this->_mockClient->contact['groups'], '0');
 		$this->receive("BPR MOB Y\r\n");
 
+		//Add Luke as a contact
 		$this->receive("LST luke@rebels.org Luke 3 1\r\n");
 		$this->assertEquals($this->_mockClient->contact['user'], 'luke@rebels.org');
 		$this->assertEquals($this->_mockClient->contact['nick'], 'Luke');
@@ -118,12 +119,35 @@ class ProtocolTest extends PHPUnit_Framework_TestCase {
 		$this->send("CHG 9 NLN 0\r\n");
 		$this->receive("CHG 9 NLN 0\r\n");
 
+		//Initial Presence
+
+		$this->receive("ILN 9 NLN emperor@empire.com Emperor 24\r\n");
+		$this->receive("ILN 9 IDL luke@rebels.org Luke 268435492\r\n");
+
+		//Challenger
 		$this->receive("CHL 0 29409134351025259292\r\n");
 		$this->send("QRY 10 msmsgs@msnmsgr.com 32\r\nd0c1178c689350104350d99f8c36ed9c");
 
-		//$this->receive("FLN emperor@empire.com\r\n");
-		//$this->receive("FLN emperor@empire.com\r\n");
-		//$this->receive("NLN BSY luke@rebels.org Luke JediMaster 268435492\r\n");
+		//Presence
+		$this->receive("NLN NLN luke@rebels.org Luke%20JediMaster 268435492\r\n");//Available
+		$this->receive("NLN BSY luke@rebels.org Luke%20JediMaster 268435492\r\n");//Busy
+		$this->receive("NLN IDL luke@rebels.org Luke%20JediMaster 268435492\r\n");//Idle
+		$this->receive("NLN BRB luke@rebels.org Luke%20JediMaster 268435492\r\n");//Be Right Back
+		$this->receive("NLN AWY luke@rebels.org Luke%20JediMaster 268435492\r\n");//Away
+		$this->receive("NLN PHN luke@rebels.org Luke%20JediMaster 268435492\r\n");//On the Phone
+		$this->receive("NLN LUN luke@rebels.org Luke%20JediMaster 268435492\r\n");//Out to lunch
+		$this->receive("FLN luke@rebels.org\r\n");
+
+		$this->receive("FLN emperor@empire.com\r\n");
+
+		// Call
+		$this->receive("RNG 876505971 65.54.228.15:1863 CKI 4216622.2513084 emperor@empire.com Emperor");
+		//$this->assertEquals("876505971", $this->mockCall->call);
+		//$this->assertEquals("65.54.228.15", $this->mockCall->server);
+		//$this->assertEquals(1863, $this->mockCall->port);
+		//$this->assertEquals("4216622.2513084", $this->mockCall->cki);
+		//$this->assertEquals("emperor@empire.com", $this->mockCall->username);
+		//$this->assertEquals("Emperor", $this->mockCall->nick);
 	}
 }
 
